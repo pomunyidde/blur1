36e9b480c6712986dd26b76430d1f3b4ca81eee3
peter omunyidde <pomunyiddde@gmail.com>
2019-09-10 18:59:02 +0000

    image blur1

--- blur1.rb
+++ blur1.rb
@@ -1,1 +1,21 @@
-puts "hello"
+class Image
+
+  def initialize(array)
+    @image = array
+  end
+
+  def output_image
+    @image.each do |column|
+      puts column.join
+    end
+  end
+
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
